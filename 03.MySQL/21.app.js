6eb72e9fa0ff1a3e4bca452bbbddd99000a42c25
Hohyeon-Jo <77525562+Hohyeon-Jo@users.noreply.github.com>
2022-09-30 10:29:58 +0900

    Add files via upload

--- 03.MySQL/21.app.js
+++ 03.MySQL/21.app.js
@@ -1,29 +1,28 @@
 const http = require('http');
 const url = require('url');
-const template = require('./view/template_bs5');
 const mysql = require('mysql');
 const config = require('./mysql.json');
+const template = require('./view/template');
 
 http.createServer((req, res) => {
     let pathname = url.parse(req.url).pathname;
-    let query = url.parse(req.url, true).query;
     switch(pathname) {
-    case '/':
+    case '/':                   // 초기 홈 화면
         const conn = mysql.createConnection(config);
         conn.connect();
-        const sql = `SELECT * FROM tigers where isDeleted=0;`;
+        const sql = `SELECT * FROM tigers WHERE isDeleted=0;`;
         conn.query(sql, (err, rows, fields) => {
             if (err)
                 throw err;
-            const trs = template.trsGen(rows);
-            const html = template.home(trs);
+            let trs = template.trsGen(rows);
+            let html = template.home(trs);
             res.end(html);
         });
         conn.end();
         break;
     default:
         res.writeHead(404, {'Content-Type': 'text/html'});
-        res.end();
+        res.end();        
     }
 }).listen(3000, () => {
     console.log('Server running at http://localhost:3000');
